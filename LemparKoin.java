74e92da2feac1f26bcd73e3ffb0df6d4d30a99b2
andrianggara12 <andri.anggara@ymail.com>
2015-06-11 17:30:00 +0700

    Create LemparKoin.java

---
+++ LemparKoin.java
@@ -0,0 +1,35 @@
+import java.util.*;
+public class LemparKoin{
+	public static void main(String[] argx){
+		int User,PC,Koin;
+		int Angka = 0;
+		int Gambar = 1;
+		Scanner scn = new Scanner(System.in);
+			System.out.print("masukkan pilihan anda(1 untuk Gambar, 0 untuk angka):...");
+				User=scn.nextInt();
+				
+				if(User>=0){
+				
+					System.out.println ("User Memilih Angka");
+					System.out.print ("PC Memilih Gambar") ;
+					
+				}
+				else{
+					System.out.println ("User Memilih Gambar ") ;
+					System.out.print ("PC Memilih Angka") ;
+				
+				}
+				
+		
+	
+	
+	
+	
+	
+	}
+
+
+
+
+
+}
